bb337273d6c8fe2444c5d3cffee8a9aee254526c
Darshan-Virkar29 <darshan.virkar06@gmail.com>
2026-06-16 20:13:55 +0530

    Add files via upload

---
+++ A2program.py
@@ -0,0 +1,31 @@
+
+# Assignment no 2
+#Question no 1
+
+no = 11
+print("Value of no is :",no)
+print("Type of no is :",type(no))
+print("Memory address of no is :",id(no))
+
+#Question no 6
+
+print("Enter the First no :")
+no1 = int(input())
+
+print("Enter the Second no :")
+no2 = int(input())
+
+print("Addition is :", no1 + no2)
+print("Subtraction is :", no1 - no2)
+print("Multiplication is :", no1 * no2)
+print("Division is :", no1 / no2)
+
+#Question no 9
+
+print("Enter your name :")
+name = input()
+
+print("Enter your age :")
+age = int(input())
+
+print("Hello", name + ", you will turn", age + 1, "next year.")
